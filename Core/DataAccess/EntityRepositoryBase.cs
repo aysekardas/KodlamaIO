3f34bda8c760ebc5e2b9ec1a8eb252f9e5827e0d
aysekardas <aysekardas7@gmail.com>
2024-02-22 19:55:20 +0300

    add entity repository base

---
+++ Core/DataAccess/EntityRepositoryBase.cs
@@ -0,0 +1,39 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess
+{
+    public class EntityRepositoryBasee<TEntity, TEntityId, TContext> : IEntityRepositoryBase<TEntity, TEntityId>
+         where TEntity : Entity<TEntityId>, new() where TContext :DbContext
+    {
+        public TEntity Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Delete(TEntity entity, bool isSoftDelete = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity? Get(Func<TEntity, bool> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<TEntity> GetList(Func<TEntity, bool>? predicate = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Core/DataAccess/IEntityRepositoryBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess
+{
+    public interface IEntityRepositoryBase<TEntity, TEntityId>
+    {
+        public IList<TEntity> GetList(Func<TEntity, bool>? predicate = null);
+        public TEntity? Get(Func<TEntity, bool> predicate);
+        public TEntity Add(TEntity entity);
+        public TEntity Update(TEntity entity);
+        public TEntity Delete(TEntity entity, bool isSoftDelete = true);
+    }
+}
