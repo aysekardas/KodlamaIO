9577f10a46c8f7bdb7ae695b3bf485f6fbecfda8
aysekardas <aysekardas7@gmail.com>
2024-02-22 19:45:53 +0300

    add entities

---
+++ Core/Entities/IEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public interface IEntity<TId>
+    {
+        public TId Id { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdateAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
---
+++ Entities/Concrete/Course.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Course :Entity<int>
+    {
+        public Course()
+        {
+            
+        }
+        public string Title { get; set; }
+        public ICollection<Category> Categories { get; set; }
+        public Instructor Instructor { get; set; }
+    }
+}
